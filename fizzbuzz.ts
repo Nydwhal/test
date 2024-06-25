38876794f600acbd74558ff1ecf732c5f6585aa9
Nydwhal <cyril.cauquil@gmail.com>
2024-06-25 11:34:11 +0200

    feat for 3 and 5

--- fizzbuzz.ts
+++ fizzbuzz.ts
@@ -1,4 +1,7 @@
 export const fizzbuzz = (number: number): string => {
+  if (number % 3 === 0 && number % 5 === 0) {
+    return "FizzBuzz";
+  }
   if (number % 3 === 0) {
     return "Fizz";
   } else if (number % 5 === 0) {
