952dcd8308b39e38f38aae64d7213dce18fa5fde
Nydwhal <cyril.cauquil@gmail.com>
2024-06-25 11:35:09 +0200

    refacto

--- fizzbuzz.ts
+++ fizzbuzz.ts
@@ -1,8 +1,7 @@
 export const fizzbuzz = (number: number): string => {
   if (number % 3 === 0 && number % 5 === 0) {
     return "FizzBuzz";
-  }
-  if (number % 3 === 0) {
+  } else if (number % 3 === 0) {
     return "Fizz";
   } else if (number % 5 === 0) {
     return "Buzz";
