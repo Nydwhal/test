8466dbe43d2dc2f70c1033d75038b6af3e7f9078
Nydwhal <cyril.cauquil@gmail.com>
2024-06-25 11:31:43 +0200

    unit test for 5

--- fizzbuzz.spec.ts
+++ fizzbuzz.spec.ts
@@ -2,7 +2,10 @@ import { describe, expect, test } from "@jest/globals";
 import { fizzbuzz } from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
-  test("fizzbuzz", () => {
+  test("multiple of 3 give Fizz", () => {
     expect(fizzbuzz(3)).toBe("Fizz");
+  });
+  test("multiple of 5 give Buzz", () => {
+    expect(fizzbuzz(5)).toBe("Buzz");
   });
 });
